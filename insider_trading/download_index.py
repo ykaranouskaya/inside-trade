cfbcc6973d1516f1dab6b7a6a638fdfafde080e1
Yuliya Karanouskaya <y.karanouskaya@gmail.com>
2018-09-15 21:36:23 +0900

    Add time span download

--- insider_trading/download_index.py
+++ insider_trading/download_index.py
@@ -1,6 +1,7 @@
 # !usr/bin/python
 
 from datetime import datetime, timedelta
+import time
 import urllib.request as request
 import urllib.parse
 from pathlib import Path
@@ -19,6 +20,22 @@ def get_quarter(date):
     return quarter[quarter_ind]
 
 
+def find_weekdays(start_date, end_date):
+    """Find weekdays from the span (start_date, end_date) `start_date` exclusive."""
+    # start_date = start_date.date()
+    # end_date = start_date.date()
+
+    date = start_date
+    weekdays = []
+    while date != end_date:
+        date += timedelta(1)
+        # print(f"New date: {date.weekday()}")
+        if date.isoweekday() in range(1, 6):
+            weekdays.append(date)
+
+    return weekdays
+
+
 def download_day_form_index(date, output_folder):
     """
     Downloads forms for a specific day and save to `output_folder`
@@ -31,11 +48,11 @@ def download_day_form_index(date, output_folder):
     stamp = ''.join([year, month, day])
     filename = f'form.{stamp}.idx'
     url_stem = '/'.join([year, quarter, filename])
-    url = urllib.parse.urljoin(DAILY_INDEX_ENDPOINT, url_stem)
-    print(url)
+    url_address = urllib.parse.urljoin(DAILY_INDEX_ENDPOINT, url_stem)
+    print(url_address)
 
     try:
-        req = request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        req = request.Request(url_address, headers={'User-Agent': 'Mozilla/5.0'})
         with request.urlopen(req) as url_in:
             data = url_in.read()
     except urllib.error.HTTPError as e:
@@ -48,11 +65,24 @@ def download_day_form_index(date, output_folder):
     except FileNotFoundError:
         print("No such output folder!")
 
-    return url
+    return url_address
+
+
+def download_span_indices(date_span, output_folder):
+    """
+    Donwload indicies for a time span (start_date, end_date] inclusive to `output_folder`.
+    :param date_span: tuple of datetime.dates (start_date, end_date)
+    :param output_folder:
+    """
+    # Find valid weekdays (omit weekends)
+    days = find_weekdays(date_span[0], date_span[1])
+    for day in days:
+        download_day_form_index(day, output_folder)
 
 
 if __name__ == "__main__":
     date = datetime.now()
     print(f"DATE: {date - timedelta(1)}")
-    url = download_day_form_index(date - timedelta(1), 'data')
-    print(f"URL: {url}")
+    # url = download_day_form_index(date - timedelta(1), 'data')
+    # print(f"URL: {url}")
+    # download_span_indices((date - timedelta(10), date), 'data')
