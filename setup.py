0f24ab9a1a887c8370cef80c132239f1a9f25ea0
Yuliya Karanouskaya <y.karanouskaya@gmail.com>
2018-09-19 23:09:57 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+VERSION = '0.0.1'
+
+NAME = 'insider_trading'
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='',
+    url='',
+    author='Yuliya Karanouskaya',
+    author_email='y.karanouskaya@gmail.com',
+    install_requires=[
+        "beautifulsoup4",
+    ]
+)
