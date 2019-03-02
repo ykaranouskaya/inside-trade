b7453f88af02612464b576df6f564d2228deee2f
Yuliya Karanouskaya <y.karanouskaya@gmail.com>
2019-03-02 22:33:18 +0900

    FEAT: add API class

--- insider_trading/market_api.py
+++ insider_trading/market_api.py
@@ -7,7 +7,6 @@ import asyncio, aiohttp
 from asyncio import Semaphore
 import urllib
 
-
 BASE_URL = "https://www.alphavantage.co"
 ENDPOINT = "/query"
 
@@ -20,10 +19,11 @@ LOG = logging.getLogger(__name__)
 
 class API:
 
-    def __init__(self, function):
+    def __init__(self, function, outputsize=None):
         self.url = urllib.parse.urljoin(BASE_URL, ENDPOINT)
         self.limit_wait = RATE_LIMIT_WAIT
         self.function = function
+        self.outputsize = outputsize
         self.API_KEY = API_KEY
 
     async def _async_request(self, params):
@@ -32,8 +32,9 @@ class API:
             async with aiohttp.ClientSession() as session:
                 async with session.get(self.url, params=params) as url:
                     data = await url.read()
+                    data = json.loads(data)
         except aiohttp.web.HTTPError as e:
-            LOG.exception(f'Error while downloading form: {e}')
+            LOG.exception(f'Error while downloading {params["symbol"]} data: {e}')
             return None
 
         return data
@@ -48,18 +49,24 @@ class API:
         parameters = {"function": self.function,
                       "symbol": symbol,
                       "apikey": self.API_KEY}
+        if self.outputsize:
+            parameters.update({'outputsize': self.outputsize})
         info = await self.request(parameters, limiter)
-        return info
+        if info.get('Error Message'):
+            LOG.warning(f'Error while downloading {symbol} data: {info.get("Error Message")}')
+            return None
 
+        LOG.info(f'Got {symbol} market data.')
+        return info
 
     def get_symbols_data(self, symbols):
 
         async def get_contents(symbols, limiter):
             coros = [self.get_symbol_info(sym, limiter) for sym in symbols]
             contents = await asyncio.gather(*coros)
+            valid_contents = [c for c in contents if c]  # Filter only valid data
 
-            return contents
-
+            return valid_contents
 
         loop = asyncio.get_event_loop()
         limiter = Semaphore(MAX_REQUESTS_PER_SEC, loop=loop)
@@ -67,4 +74,3 @@ class API:
         symbols_data = loop.run_until_complete(get_contents(symbols, limiter))
 
         return symbols_data
-
